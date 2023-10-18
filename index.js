411627ea2b662adfeb99445ec9a9c831fb17ef22
Merima Dedic <merimaxdedic@gmail.com>
2023-10-17 22:59:28 -0500

    Adds index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase()) ;
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase( ));
+}
+function sayHiToHeadphonedRoommate(message) {
+  if (message === message.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (message === message.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (message === "Let's have dinner together!") {
+    return "I would love to!";
+  }
+}
